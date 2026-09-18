add162d02fc08d39b0f9b5547e3a1e7e332fa1f8
matiasfEt12d1 <matias.floret12d1@gmail.com>
2026-09-18 11:59:29 -0300

    update: add method
    ejecutar turno async

--- src/Aplicacion/Interfaces/IBatallaServicio.cs
+++ src/Aplicacion/Interfaces/IBatallaServicio.cs
@@ -3,6 +3,7 @@ namespace Aplicacion.Interfaces;
 public interface IBatallaServicio
 {
     Task<int> CrearBatallaAsync(int atacanteId, int defensorId);
+    Task<(double Danio, bool EsCritico)> EjecutarTurnoPolimorficoAsync(int personajeAtacanteId, int personajeDefensorId, int? habilidadId = null);
     Task<int> IniciarBatallaAsync(int personaje1Id, int personaje2Id);
     Task ProcesarFinalBatallaAsync(int batallaId, int ganadorId, int perdedorId, double vidaRestante, int totalRondas, double ultimoGolpe);
 }
