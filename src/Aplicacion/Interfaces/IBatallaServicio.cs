d64f9f61b2a73a675daebb743678f853c1ce5fd8
matiasfEt12d1 <matias.floret12d1@gmail.com>
2026-09-18 11:53:25 -0300

    update: add create battle

--- src/Aplicacion/Interfaces/IBatallaServicio.cs
+++ src/Aplicacion/Interfaces/IBatallaServicio.cs
@@ -2,6 +2,7 @@ namespace Aplicacion.Interfaces;
 
 public interface IBatallaServicio
 {
+    Task<int> CrearBatallaAsync(int atacanteId, int defensorId);
     Task<int> IniciarBatallaAsync(int personaje1Id, int personaje2Id);
     Task ProcesarFinalBatallaAsync(int batallaId, int ganadorId, int perdedorId, double vidaRestante, int totalRondas, double ultimoGolpe);
 }
