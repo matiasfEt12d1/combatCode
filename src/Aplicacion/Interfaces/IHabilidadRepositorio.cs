e8c43e29022d11846d25fb2260f7215c0d0fa905
matiasfEt12d1 <matias.floret12d1@gmail.com>
2026-09-18 10:33:28 -0300

    update: archives moved

--- src/Aplicacion/Interfaces/IHabilidadRepositorio.cs
+++
@@ -1,9 +0,0 @@
-using Persistencia.Entidades;
-
-namespace Aplicacion.Interfaces;
-
-public interface IHabilidadRepositorio
-{
-    Task<IEnumerable<HabilidadTabla>> ObtenerPorTipoPersonajeAsync(string tipoPersonaje);
-    Task<HabilidadTabla?> ObtenerPorIdAsync(int id);
-}
